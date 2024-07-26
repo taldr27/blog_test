ab55554a203cb98bdbdd8ef455c7624f8675e7a9
taldr27 <diegogarciaq98@gmail.com>
2024-07-26 13:40:40 -0500

    Refactor to avoid multiple calls to supabase

--- src/app/components/MainPage.tsx
+++ src/app/components/MainPage.tsx
@@ -1,30 +1,8 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 import PostsComponent from "./PostsComponent";
-import YourPosts from "./YourPosts";
 import Separator from "./Separator";
-import InputBox from "./InputBox";
 import AddNewPost from "./AddNewPost";
 
-export default async function MainPage({ user }: any) {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: posts } = await supabase
-    .from("posts")
-    .select("*, profile:profiles(*)")
-    .order("created_at", { ascending: false });
-
-  // TODO: I think I can filter posts to not repeat with the general posts section.
-  let userPosts = [];
-  if (user) {
-    const { data: userPostsData } = await supabase
-      .from("posts")
-      .select("*, profile:profiles(*)")
-      .eq("profile_id", user.id)
-      .order("created_at", { ascending: false })
-      .limit(4);
-    userPosts = userPostsData || [];
-  }
-
+export default function MainPage({ user, posts, userPosts }: any) {
   return (
     <div className="max-w-[1200px] w-full self-center">
       <Separator title="Available Posts" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,26 @@ export default async function Home() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data: posts } = await supabase
+    .from("posts")
+    .select("*, profile:profiles(*)")
+    .order("created_at", { ascending: false });
+
+  // TODO: I think I can filter posts to not repeat with the general posts section.
+  let userPosts = [];
+  if (user) {
+    const { data: userPostsData } = await supabase
+      .from("posts")
+      .select("*, profile:profiles(*)")
+      .eq("profile_id", user.id)
+      .order("created_at", { ascending: false })
+      .limit(4);
+    userPosts = userPostsData || [];
+  }
+
   return (
     <div className="w-full max-w-[1289px] px-8 flex flex-col">
-      <MainPage user={user} />
+      <MainPage posts={posts} user={user} userPosts={userPosts} />
     </div>
   );
 }
