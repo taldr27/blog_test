dc22e09792478b4e1775a2b5d554ccd3a22d0885
taldr27 <diegogarciaq98@gmail.com>
2024-07-26 20:27:20 -0500

    Refactor in actions file to separate concerns

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -4,12 +4,9 @@ import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
-export const addPost = async (formData: FormData) => {
-  const title = formData.get("title") as string;
-  const content = formData.get("content") as string;
-  const imageFile = formData.get("image") as File | null;
+const supabase = createServerActionClient({ cookies });
 
-  const supabase = createServerActionClient({ cookies });
+const authenticateUser = async () => {
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -18,35 +15,80 @@ export const addPost = async (formData: FormData) => {
     console.error("User not authenticated");
     throw new Error("User not authenticated");
   }
+  return user;
+};
 
-  let imageUrl = "";
+const uploadImage = async (
+  userId: string,
+  imageFile: File | null,
+  folder: string
+) => {
+  if (!imageFile) return "";
 
-  if (imageFile) {
-    const { data: uploadData, error: uploadError } = await supabase.storage
-      .from("blog_images")
-      .upload(`posts/${user.id}/${imageFile.name}`, imageFile);
+  const { data: uploadData, error: uploadError } = await supabase.storage
+    .from("blog_images")
+    .upload(`${folder}/${userId}/${imageFile.name}`, imageFile);
 
-    if (uploadError) {
-      throw new Error(`Error uploading image: ${uploadError.message}`);
-    }
-
-    const {
-      data: { publicUrl },
-    } = supabase.storage
-      .from("blog_images")
-      .getPublicUrl(uploadData?.path || "");
-
-    imageUrl = publicUrl || "";
+  if (uploadError) {
+    throw new Error(`Error uploading image: ${uploadError.message}`);
   }
 
-  const { error } = await supabase
-    .from("posts")
-    .insert({ title, content, profile_id: user.id, image_url: imageUrl });
+  const {
+    data: { publicUrl },
+  } = supabase.storage.from("blog_images").getPublicUrl(uploadData?.path || "");
 
+  return publicUrl || "";
+};
+
+const insertData = async (table: string, data: any) => {
+  const { error } = await supabase.from(table).insert(data);
   if (error) {
-    console.error("Error adding new post", error);
-    throw new Error(`Error adding new post: ${error.message}`);
+    console.error(`Error adding new ${table}`, error);
+    throw new Error(`Error adding new ${table}: ${error.message}`);
+  }
+};
+
+export const addPost = async (formData: FormData) => {
+  const title = formData.get("title") as string;
+  const content = formData.get("content") as string;
+  const imageFile = formData.get("image") as File | null;
+
+  const user = await authenticateUser();
+
+  if (!content && !imageFile) {
+    throw new Error("Post must have content or an image");
   }
 
+  const imageUrl = await uploadImage(user.id, imageFile, "posts");
+
+  await insertData("posts", {
+    title,
+    content,
+    profile_id: user.id,
+    image_url: imageUrl,
+  });
   revalidatePath("/");
+};
+
+export const addComment = async (formData: FormData) => {
+  const content = formData.get("content") as string;
+  const imageFile = formData.get("image") as File | null;
+  const postId = formData.get("postId") as string;
+
+  const user = await authenticateUser();
+
+  if (!content && !imageFile) {
+    throw new Error("Comment must have content or an image");
+  }
+
+  const imageUrl = await uploadImage(user.id, imageFile, "comments");
+
+  await insertData("comments", {
+    content,
+    profile_id: user.id,
+    image_url: imageUrl,
+    post_id: postId,
+  });
+
+  revalidatePath(`/posts/${postId}`);
 };
